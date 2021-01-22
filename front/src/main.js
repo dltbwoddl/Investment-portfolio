fa5945aea5c8da7a42365d41ebd935eba858cab0
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2021-01-22 13:40:47 +0900

    0122
    0122

--- front/src/main.js
+++ front/src/main.js
@@ -5,9 +5,17 @@ import store from './store'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BlackB from './components/BlackButton.vue'
+import ModifyB from './components/ModifyButton.vue'
+import GrayB from './components/GrayButton.vue'
+import Plusb from './components/PlusButton.vue'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.component('BlackB',BlackB)
+Vue.component('ModifyB',ModifyB)
+Vue.component('GrayB',GrayB)
+Vue.component('Plusb',Plusb)
 
 Vue.config.productionTip = false
 new Vue({
