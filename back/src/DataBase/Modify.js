6d9a8fe61f3c06a27fb215cc284de806f537a656
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2021-02-17 14:16:50 +0900

    0217
    0217

--- back/src/DataBase/Modify.js
+++ back/src/DataBase/Modify.js
@@ -6,14 +6,36 @@ var async = require('async');
 module.exports = {
     HabitListModify: function (req, res) {
         var data = req.body;
+        console.log(data)
         const pool_1 = new pool();
         Promise.using(pool_1.connect(), conn => {
-            conn.queryAsync(`SELECT id FROM habitlist`).then((ret) => {
+            conn.queryAsync(`SELECT * FROM habitlist`).then((ret) => {
                 return ret
             }).then((ret) => {
                 Promise.using(pool_1.connect(), conn => {
-                    //추가하기.        
                     var ids = Object.keys(data);
+                    console.log(ret)
+                    //수정하기.
+                    var UpdateData=ids.filter(e =>e<ret[ret.length-1].id)
+                    console.log(UpdateData)
+                    var UpdateDataQuery_1=''
+                    var UpdateDataQuery_2=''
+                    // ret.forEach((d)=>{
+                    //     if(data[d.id]!=d.habit){
+                    //         UpdateDataQuery_1+=`\n WHEN id=id=${d.id} THEN '${data[d.id]}'`
+                    //     }
+                    // })
+                    UpdateData.forEach((id)=>{
+                        if(data[id]!=ret[id-1].habit){
+                            UpdateDataQuery_1+=`\n WHEN id=${id} THEN '${data[id]}'`
+                            UpdateDataQuery_2+=`${id},`
+                        }
+                    })
+                    conn.queryAsync(`UPDATE habitlist SET habit = CASE ${UpdateDataQuery_1} END WHERE id IN(${UpdateDataQuery_2.slice(0,-1)})`).then(()=>{
+                        console.log('habitlist update success')
+                    })
+
+                    //추가하기.        
                     var PlusDataIds=ids.filter(e =>e>ret[ret.length-1].id)
                     PlusQuery = ''
                     PlusDataIds.forEach((id)=>{
@@ -24,11 +46,9 @@ module.exports = {
                     if(PlusQuery.length !=0){
                         conn.queryAsync(`INSERT INTO habitlist (habit) VALUES ${PlusQuery}`)
                         .then(()=>{
-                            console.log('success')
+                            console.log('habitlist plus success')
                         })
                     }
-                    //수정하기.
-
                     //삭제하기.
 
                 })
